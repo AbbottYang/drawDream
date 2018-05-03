01759daad1ec0faf1fd32079ecfbfce39c097183
杨子航 <1848618605@qq.com>
2018-05-04 00:50:39 +0800

    first

---
+++ web.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>yzh的网站</title>
+
+        <style type="text/css">
+        h1{
+         font-size:50px;
+          color:#930;
+          text-align:center;
+
+        }
+		p100{
+			color:#AD6C6D
+		}
+		p{
+			font-size:50px;
+			color:#CC6263
+		}
+        body p100 {
+}
+div#div1{ 
+position:absolute;
+top:00; 
+left:0; 
+bottom:0; 
+right:0; 
+z-index:-1; 
+} 
+div#div1 > img { 
+height:100%; 
+width:100%; 
+border:0; 
+
+} 
+div#div2{
+	position: absolute;
+	top: 653px;
+	left: -4px;
+	bottom: 0px;
+	right: 50px;
+	z-index: 0;
+	width: 443px;
+} /*
+div#div2 > img { 
+height:10%; 
+width:10%; 
+border:0px; 
+
+} */
+        </style>
+</head>
+
+<body>
+<div id="div1"><img src="5877343335fd2.jpg" width="440" height="274" /></div>
+<?php
+    echo "I love u";
+?>
+<h1> Welcome to my website</h1>
+<p100> &nbsp;&nbsp;&nbsp;    <a href="3.html"> 跳转 </a>
+
+</p100>
+<p> 6666666666</p>   
+<p> 6666666666</p>
+<p> 6666666666</p>
+<p> 6666666666</p>
+<p>&nbsp;</p>
+<p>
+<div id="div2"><img src="u=383617309,2159165294&fm=117&gp=0.jpg" width="440" height="274" /></div></p>
+</body>
+ 
+</html>
